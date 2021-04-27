43622c0ec37e8ab95824d7f122bdc940c3c88768
Jacob Reinhold <jacob.reinhold@jhu.edu>
2021-04-27 17:24:34 -0400

    add version and author info to top-level import

--- niftidataset/__init__.py
+++ niftidataset/__init__.py
@@ -1,3 +1,7 @@
+__author__ = """Jacob C Reinhold"""
+__email__ = 'jcreinhold@gmail.com'
+__version__ = '0.2.1'
+
 from niftidataset.errors import *
 from niftidataset.utils import *
 from niftidataset.dataset import *
